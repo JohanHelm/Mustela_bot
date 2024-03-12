425fb3cf90920c17405c846d0873e82493fb660f
johan_helm <jou@ngs.ru>
2024-03-12 15:00:10 +0000

    start logging

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 import config as cfg
-import logging
+from logging_settings import configure_logger, init_logger
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 #from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
@@ -14,4 +14,7 @@ bot = Bot(token=cfg.TOKEN, parse_mode="HTML")
 # Диспетчер для бота
 dp = Dispatcher(bot, storage=stor)
 # Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
+logger = init_logger()
+configure_logger(logger, "logs/logfile.log", rotation=10)
+
